3e6f1a00b8d8bb1c5c689098f06f3e695c131472
Zach Goldman <elevatorgoldman@aol.com>
2014-02-23 05:48:43 +0000

    check balance

--- ripbot.rb
+++ ripbot.rb
@@ -4,9 +4,17 @@ require 'rubygems'
 require 'chatterbot/dsl'
 require 'doge_api'
 
+$my_api_key = 'API_KEY'
+$my_address = 'DBv97MSG4gNjcGoHCMX1nfanLjzBgVERi9'
+$my_api_address = 'DPf8M6McXUZW5zDxwAKXAfid4A78P8o21M'
+
+doge_api = DogeApi::DogeApi.new($my_api_key)
+
 puts "Loading search_bot.rb"
 
 search("twitter", :lang => "en") do |tweet|
-  puts tweet[:text]
+  if (doge_api.get_balance < 0)
+    break
+  end  
 end
 
